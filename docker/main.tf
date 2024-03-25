82ec494b723f181a277d9de32a657a972b958b30
HaoWang95 <alanwang0028@gmail.com>
2024-03-25 17:05:57 +1100

    update: docker terraform

---
+++ docker/main.tf
@@ -0,0 +1,27 @@
+# what does the terraform declaration mean?
+terraform {
+  required_providers {
+    docker = {
+        source = "kreuzwerker/docker"
+        version = "~>3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+
+}
+
+resource "docker_image" "nginx" {
+    name = "nginx"
+    keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.image_id
+    name = "Tutorial"
+    ports {
+        internal = 80
+        external = 8000
+    }
+}
